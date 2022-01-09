75e7bce4df913b2d8f158011339ab0870be915ba
FMFARDIN <93036193+FMFARDIN@users.noreply.github.com>
2022-01-09 18:51:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from tkinter import *
 from tkinter import colorchooser
 import pyperclip
+
+# Window attributes
 root = Tk()
 root.geometry("400x300")
 root.iconbitmap("color_picker.ico")
 root.title(" Simple Color Picker")
+
+# Function
 def color():    
     color = colorchooser.askcolor()[1]
     label = Label(root, text=color).pack()
     pyperclip.copy(color)   
 
+# Upper text
 text = Label(root, text="Choose a color it will be copied to your clipboard automatically", fg="#828282").pack()
-btn = Button(root, text="Choose a color",bg="#0091ff",activebackground="#00aeff",borderwidth="10", bd=0, font="Ubuntu", command=color).pack(pady="100",padx="90")
 
+# Choose color button
+btn = Button(root, text="Choose a color",bg="#0091ff",activebackground="#00aeff",borderwidth="10", bd=0, font="Ubuntu", command=color).pack(pady="100",padx="90")
 
 mainloop()
