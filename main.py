0dfef72f82c4fa085390bd72187d128988421dc4
FMFARDIN <93036193+FMFARDIN@users.noreply.github.com>
2022-01-09 20:33:38 +0600

    1.1 Commit
    New label features

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import pyperclip
 
 # Window attributes
 root = Tk()
-root.geometry("400x300")
 root.iconbitmap("color_picker.ico")
 root.title(" Simple Color Picker")
 
 # Function
 def color():    
     color = colorchooser.askcolor()[1]
-    label = Label(root, text=color).pack()
+    label = Label(root, text=color, bg=color, font="Ubuntu, 12").pack(padx=5,pady=10)
     pyperclip.copy(color)   
 
 # Upper text
-text = Label(root, text="Choose a color it will be copied to your clipboard automatically", fg="#828282").pack()
+text = Label(root, text="Choose a color it will be copied to your clipboard automatically", fg="#828282").pack(pady=10,padx=10)
 
 # Choose color button
 btn = Button(root, text="Choose a color",bg="#0091ff",activebackground="#00aeff",borderwidth="10", bd=0, font="Ubuntu", command=color).pack(pady="100",padx="90")
 
 mainloop()
+
